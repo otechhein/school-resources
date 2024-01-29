62b0c90a82fbb143963c0ee052fde240733c14b5
heinwithsmile <heinhtetaung7715@gmail.com>
2024-01-29 09:46:34 +0630

    Defined name to MiniCalculator Assignment for OOP

--- 07-php/3rd-week/OOP/Namespace-Module/index.php
+++ 07-php/3rd-week/OOP/Namespace-Module/index.php
@@ -4,7 +4,7 @@
 
 /**
  * 1. Class, Namespace, Interface, Traits အမည်တွေက Capital Case
- * 2. Namespace လမ်းကြောင်း နဲ့  Folder Path တူသင့်တယ်
+ * 2. Namespace လမ်းကြောင်း နဲ့  Project Folder Path တူသင့်တယ်
  * 3. Code File name နဲ့ Class name နဲ့ တူသင့်တယ်
  */
 

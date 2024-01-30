014373f80f84051629053568abca49729bcefbff
heinwithsmile <heinhtetaung7715@gmail.com>
2024-01-30 12:00:27 +0630

    removed commented code

--- 07-php/4th-week/Mysql-PHP/mysqli/procedural/index.php
+++ 07-php/4th-week/Mysql-PHP/mysqli/procedural/index.php
@@ -2,12 +2,6 @@
 
 $conn = mysqli_connect('localhost', 'root', '', 'demo');
 
-// echo '<pre>';
-// var_dump($conn);
-// echo '</pre>';
-// exit;
-// Check Connection
-
 if(mysqli_connect_errno()){
     // Connection Failed
     echo 'Failed to connect to MySQL' . mysqli_connect_errno();
